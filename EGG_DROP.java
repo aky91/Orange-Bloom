fdcc1fbae4aebde8c1fa2ca811428193e4264b26
AKASH YADAV <aakash28399@gmail.com>
2018-05-24 23:55:24 +0530

    Create EGG_DROP.java

---
+++ EGG_DROP.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+class Ideone{
+
+	//https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle/0
+
+	public static Scanner scn = new Scanner(System.in);
+
+	public static void main (String[] args) throws java.lang.Exception{
+
+		int T = scn.nextInt();
+
+		while(T-- > 0){
+
+			int N = scn.nextInt();
+            int K = scn.nextInt();
+            
+            int ans = getAns(N, K);
+
+		}	
+
+	}
+	
+	public static int getAns(int N, int K){
+	    
+	    if(N == 0 || K == 0 || K == 1)
+	        return K;
+	        
+	    int min = Integer.MAX_VALUE;
+	    
+	    for(int i = 1; i <= K; i++)
+	        min = Math.min(min, (1 + Math.max(getAns(N, K-i), getAns(N-1,K-1))));
+	
+	    return min;
+	}
+}
