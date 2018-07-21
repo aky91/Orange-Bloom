6da2a882150df4342f494611736dc73160878059
AKASH YADAV <aakash28399@gmail.com>
2018-07-22 00:36:30 +0530

    Update Max Distance.java

--- Max Distance.java
+++ Max Distance.java
@@ -14,23 +14,47 @@ public class Solution {
         
         int N = A.size();
         
-        int max = 0;
         
-        int[] index = new int[N];
         ArrayList<Type> B = new ArrayList<Type>();
         
         for(int i = 0; i < N; i++)
             B.add(new Type(A.get(i), i));
         
-        Collections.sort(B, new Comparator<Type>);
-        
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                if(A.get(i) <= A.get(j) && (j - i) > max)
-                    max = j - i;
+        Collections.sort(B, new Comparator<Type>(){
+            
+            public int compare(Type a, Type b){
+                
+                return a.data - b.data;
             }
+        });
+        
+        int[] preFix = new int[N];
+        
+        int max = 0;
+        
+        for(int i = N - 2; i >= 0; i--){
+            
+            if(B.get(i+1).prev > max)
+                max = B.get(i+1).prev;
+                
+            preFix[i] = max;
         }
         
-        return max;
+        int ans = 0;
+        
+        for(int idx = 0; idx < N; idx++){
+            
+            int i = B.get(idx).prev;
+            
+            int j = preFix[idx];
+            
+            if(j < i)
+                continue;
+            
+            if(j - i > ans)
+                ans = j - i;
+        }
+        
+        return ans;
     }
 }
